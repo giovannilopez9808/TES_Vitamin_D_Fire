d86fe074df53f91125797eab0ec864ae271519e4
Giovanni <giovannilopez9808@gmail.com>
2021-07-27 02:52:38 -0500

    Add all variations of calculations

--- Scripts/Graphics_compare_doses.py
+++ Scripts/Graphics_compare_doses.py
@@ -3,37 +3,46 @@ from functions import *
 import pandas as pd
 
 
+def obtain_id_and_title_parameters(id_Ozone="", id_AOD=""):
+    AOD_dataset = select_dataset_parameters_AOD(id_AOD)
+    Ozone_dataset = select_dataset_parameters_Ozone(id_Ozone)
+    # Define ID
+    ID = "_{}_{}".format(Ozone_dataset["Filename"],
+                         AOD_dataset["Filename"])
+    title = "{} {}".format(Ozone_dataset["title"],
+                           AOD_dataset["title"])
+    return ID, title
+
+
 parameters = {"path data": "../Data/",
               "path graphics": "../Graphics/",
               "file data": "Doses_time",
-              "file OMI": "Ozone_OMI",
-              "file binary search": "binary_search",
-              "file clear sky": "clear_sky",
               "date initial": "2020-06-01",
               "date final": "2020-08-01",
-              "dataset": "1/4 MED"
+              "dataset doses": "1/4 MED",
+              # The first dataset is used for set the xticks
+              "dataset parameters": {"dataset 1": {"dataset AOD": "Binary search",
+                                                   "dataset Ozone": "OMI"},
+                                     "dataset 2": {"dataset AOD": "Binary search",
+                                                   "dataset Ozone": "260"},
+                                     "dataset 3": {"dataset AOD": "0.30",
+                                                   "dataset Ozone": "260"},
+                                     },
               }
-OMI_data = read_data(parameters["path data"],
-                     "{}_{}.csv".format(parameters["file data"],
-                                        parameters["file OMI"]))
-binary_search_data = read_data(parameters["path data"],
-                               "{}_{}.csv".format(parameters["file data"],
-                                                  parameters["file binary search"]))
-clear_sky_data = read_data(parameters["path data"],
-                           "{}_{}.csv".format(parameters["file data"],
-                                              parameters["file clear sky"]))
-months, months_names = obtain_xticks(OMI_data.index)
-plt.scatter(OMI_data.index, OMI_data[parameters["dataset"]],
-            label="Ozone OMI",
-            lw=1.5)
-plt.scatter(binary_search_data.index, binary_search_data[parameters["dataset"]],
-            label="Binary search",
-            lw=1.5)
-plt.scatter(clear_sky_data.index, clear_sky_data[parameters["dataset"]],
-            label="Clear sky",
-            lw=1.5)
-plt.xticks(months,
-           months_names)
+for i, dataset in enumerate(parameters["dataset parameters"]):
+    dataset = parameters["dataset parameters"][dataset]
+    ID, title = obtain_id_and_title_parameters(dataset["dataset Ozone"],
+                                               dataset["dataset AOD"])
+    data = read_data(parameters["path data"],
+                     "{}{}.csv".format(parameters["file data"],
+                                       ID))
+    plt.plot(data.index, data[parameters["dataset doses"]],
+             label=title,
+             lw=1.5)
+    if i == 0:
+        months, months_names = obtain_xticks(data.index)
+        plt.xticks(months,
+                   months_names)
 plt.xlim(pd.to_datetime(parameters["date initial"]),
          pd.to_datetime(parameters["date final"]))
 plt.xlabel("Periodo 2019-2020",
